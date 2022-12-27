40cc2eb73e1d6d5693c8d2d91045ebf7b7bea9f3
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-26 21:00:46 -0300

    metodos then() e json()

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-const consultaCep = fetch('viacep.com.br/ws/01001000/json/');
+const consultaCep = fetch('https://viacep.com.br/ws/44089150/json/')
+    .then(response => response.json())
+    .then(json => console.log(json));
 
 console.log(consultaCep)

f1e8109d5a3b886a2207cf92c66ee1361dcb28b9
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-26 22:34:35 -0300

    curso finalizado

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 async function buscarEndereco(cep){
     try {
+
+        var mensagemErro = document.getElementById('erro');
+        mensagemErro.innerHTML = "";
+
         const consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const consultaCepJSON = await consultaCep.json();
         if (consultaCepJSON.erro) {
             throw Error('CEP inexistente!')
         }
+
+        const estado = document.getElementById('estado');
+        const cidade = document.getElementById('cidade');
+        const bairro = document.getElementById('bairro');
+        const complemento = document.getElementById('complemento');
+        const endereco = document.getElementById('endereco');
+        cidade.value = consultaCepJSON.localidade;
+        estado.value = consultaCepJSON.uf;
+        bairro.value = consultaCepJSON.bairro;
+        complemento.value = consultaCepJSON.complemento;
+        endereco.value = consultaCepJSON.logradouro;
         console.log(consultaCepJSON);
+        return consultaCepJSON;
     } catch (error) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
         console.log(error)
     }
 }
 
-const ceps = ['44089150', '44089278', '44002384']
-const enderecos = ceps.map(cep => buscarEndereco(cep));
-Promise.all(enderecos).then(response => console.log(response))
+const cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscarEndereco(cep.value))
     

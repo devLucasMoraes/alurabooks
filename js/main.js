eee48b6b3d4c2c9922eac6848c3f7e6c3bb056b4
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-26 21:44:42 -0300

    entendendo o Promise.all()

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-async function buscarEnderco(){
+async function buscarEndereco(cep){
     try {
-        const consultaCep = await fetch('https://viacep.com.br/ws/45089150/json/');
+        const consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const consultaCepJSON = await consultaCep.json();
         if (consultaCepJSON.erro) {
             throw Error('CEP inexistente!')
@@ -11,5 +11,7 @@ async function buscarEnderco(){
     }
 }
 
-buscarEnderco()
+const ceps = ['44089150', '44089278', '44002384']
+const enderecos = ceps.map(cep => buscarEndereco(cep));
+Promise.all(enderecos).then(response => console.log(response))
     
